5d72dddd2bf9f9815488b77379e14fb79bd00d91
wboykinm <bill@billmorris.io>
2024-12-01 07:41:17 -0500

    Split components

---
+++ next/nextTracks.js
@@ -0,0 +1,92 @@
+const tracks = [
+    {
+        "order": 1,
+        "artist": "Aretha Franklin",
+        "song": "First Snow in Kokomo",
+        "url": "https://www.dropbox.com/scl/fi/9b79ruc7i5eoufuonnj2h/01-First-Snow-in-Kokomo.mp3?rlkey=4un2t3kr69litcq3pftmhb9j1&dl=1"
+    },
+    {
+        "order": 2,
+        "artist": "Bleachers",
+        "song": "Merry Christmas, Please Don't Call",
+        "url": "https://www.dropbox.com/scl/fi/0e6pfjbk27tmvqjf2h2ug/02-Merry-Christmas-Please-Don-t-Call.mp3?rlkey=skq4nfmhqyfvn8bscfperrkl5&dl=1"
+    },
+    {
+        "order": 3,
+        "artist": "Blue Highway",
+        "song": "The Northside",
+        "url": "https://www.dropbox.com/scl/fi/l3328hy9iy140tlgt97xz/03-The-North-Side.mp3?rlkey=jy9toji8e4nk8f2x053501dka&dl=1"
+    },
+    {
+        "order": 4,
+        "artist": "Okkerville River",
+        "song": "Listening to Otis Redding at Home During Christmas",
+        "url": "https://www.dropbox.com/scl/fi/ebe0drbdk1pbmrcjgwo41/04-Listening-To-Otis-Redding-At-Home-During-Christmas.mp3?rlkey=p45glfmlmipjq0fpo31dj7lij&dl=1"
+    },
+    {
+        "order": 5,
+        "artist": "Otis Redding",
+        "song": "Merry Christmas, Baby",
+        "url": "https://www.dropbox.com/scl/fi/0xi31h3s8l5p5a8tuhmhs/05-Merry-Christmas-Baby.mp3?rlkey=7x4q4d9vigj24tfsshzm9sp8c&dl=1"
+    },
+    {
+        "order": 6,
+        "artist": "11 Acorn Lane",
+        "song": "Silent Night",
+        "url": "https://www.dropbox.com/scl/fi/q71jdfqytp0aq7boel8fr/06-Silent-Night.mp3?rlkey=vbraotqnr6p72qwn9z1kxi7w8&dl=1"
+    },
+    {
+        "order": 7,
+        "artist": "Johnny Cash",
+        "song": "The Blizzard",
+        "url": "https://www.dropbox.com/scl/fi/7sb9yh1c4uj5x48dkdo8p/07-The-Blizzard.mp3?rlkey=eq954wx40n2su7fq6eiewjv5d&dl=1"
+    },
+    {
+        "order": 8,
+        "artist": "G. Love & Special Sauce",
+        "song": "Christmas Baby",
+        "url": "https://www.dropbox.com/scl/fi/78dfr53divztgh6kbz3l4/08-Christmas-Baby.mp3?rlkey=eutxaphxa4ttcg9kwpl199503&dl=1"
+    },
+    {
+        "order": 9,
+        "artist": "Death Cab for Cutie",
+        "song": "Northern Lights",
+        "url": "https://www.dropbox.com/scl/fi/9cqusumhl9832zgopd868/09-Northern-Lights.mp3?rlkey=o221u1de21qhcxphq4r73zypy&dl=1"
+    },
+    {
+        "order": 10,
+        "artist": "Ted Rosenthal Trio",
+        "song": "Snowscape",
+        "url": "https://www.dropbox.com/scl/fi/6obld5swgigpd1kznhai8/10-Snowscape.mp3?rlkey=o44pe16uag8md0wv1cvc36w6t&dl=1"
+    },
+    {
+        "order": 11,
+        "artist": "JT Donaldson",
+        "song": "Snowflakes Falling (Movement I)",
+        "url": "https://www.dropbox.com/scl/fi/i3esfrw8dcp03z9kdq466/11-JT-Donaldson-_-Snowflakes-Falling-Movement-I.mp3?rlkey=1is1ztxe1cdk0ne1b31zhdac4&dl=1"
+    },
+    {
+        "order": 12,
+        "artist": "Ennio Máno",
+        "song": "We Three Kings",
+        "url": "https://www.dropbox.com/scl/fi/yj7e1pakshnl2xusva9wj/12-We-Three-Kings-Jazz-Version.mp3?rlkey=ijoglbvob0vp34rl33vkaf9kr&dl=1"
+    },
+    {
+        "order": 13,
+        "artist": "Brandee Younger",
+        "song": "The Windmills of Your Mind",
+        "url": "https://www.dropbox.com/scl/fi/jywan75z3nggkzbex3nsn/13-The-Windmills-Of-Your-Mind-feat.-9th-Wonder.mp3?rlkey=bm34ae85fzuzuiexh8dfbhcxv&dl=1"
+    },
+    {
+        "order": 14,
+        "artist": "Loreena McKennitt",
+        "song": "The Mystic's Dream",
+        "url": "https://www.dropbox.com/scl/fi/pwa9ouvf144jvckq9x0ue/14-The-Mystic-s-Dream.mp3?rlkey=nusj7eafcchxhkhzpwamk6y9b&dl=1"
+    },
+    {
+        "order": 15,
+        "artist": "Michael Hedges",
+        "song": "Prelude to Cello Suite #1 in G Major",
+        "url": "https://www.dropbox.com/scl/fi/b6m0g6eygs4zb3fdkyi2s/15-Prelude-to-Cello-Suite-1-In-G-Major.mp3?rlkey=z0f8p6li31mxt6q76ctipat36&dl=1"
+    }
+];
